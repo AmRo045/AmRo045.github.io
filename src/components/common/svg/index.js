f2befa3e24bf53ce87cb62b2f1b06ac8bd6fb59c
AmRo <virtualemailbox045@gmail.com>
2022-07-23 21:05:35 +0430

    Recreate project using yarn and typescript template

--- src/components/common/svg/index.js
+++
@@ -1,7 +0,0 @@
-import GithubIcon from './GithubIcon';
-import InstagramIcon from './InstagramIcon';
-import Logo from './Logo';
-import StackOverFlowIcon from './StackOverFlowIcon';
-import TwitterIcon from './TwitterIcon';
-
-export { GithubIcon, InstagramIcon, Logo, StackOverFlowIcon, TwitterIcon };
