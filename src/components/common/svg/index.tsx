a945b68e98367ee1855366946ecdf2190a34afa8
AmRo <virtualemailbox045@gmail.com>
2022-07-23 22:13:38 +0430

    Copy UI from previous version

---
+++ src/components/common/svg/index.tsx
@@ -0,0 +1,7 @@
+import GithubIcon from './GithubIcon';
+import InstagramIcon from './InstagramIcon';
+import Avatar from './Avatar';
+import StackOverFlowIcon from './StackOverFlowIcon';
+import TwitterIcon from './TwitterIcon';
+
+export { GithubIcon, InstagramIcon, Avatar, StackOverFlowIcon, TwitterIcon };
