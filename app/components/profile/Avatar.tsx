c7b635f76c7823d4971fdeeed191ebd1c436ce72
AmRo <VirtualEmailBox045@Gmail.Com>
2023-10-02 19:12:35 +0330

    Decouple Profile component styles

--- app/components/profile/Avatar.tsx
+++ app/components/profile/Avatar.tsx
@@ -1,9 +1,11 @@
+import styles from "@/app/components/css/Profile.module.css";
+
 import AvatarBg from "@/app/components/svg/AvatarBg";
 import AmRoLogo from "@/app/components/svg/Avatar";
 
 const Avatar = (): JSX.Element => {
     return (
-        <div className="profile__avatar">
+        <div className={styles.avatar}>
             <AvatarBg />
             <AmRoLogo />
         </div>
--- app/components/profile/Bio.tsx
+++ app/components/profile/Bio.tsx
@@ -1,8 +1,10 @@
+import styles from "@/app/components/css/Profile.module.css";
+
 const Bio = (): JSX.Element => {
     return (
         <>
-            <div className="profile__name">Amin Rostami</div>
-            <div className="profile__description">
+            <div className={styles.name}>Amin Rostami</div>
+            <div className={styles.description}>
                 Love to learn, Love to find solution for the problems.
             </div>
         </>
