1f32f11f6eb0b896c15fefc305f604a2c673405c
AmRo <VirtualEmailBox045@Gmail.Com>
2025-04-25 08:41:50 +0330

    Remove old code

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Personal Page
-
-![Build & Deploy](https://github.com/amro045/AmRo045.github.io/actions/workflows/build-deploy.yml/badge.svg)
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    distDir: "build",
-
-    images: { unoptimized: true },
-};
-
-module.exports = nextConfig;
