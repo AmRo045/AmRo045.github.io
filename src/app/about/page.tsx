765d6bbd9332ac96b214e18e5297c955ee080b22
AmRo <VirtualEmailBox045@Gmail.Com>
2025-04-25 09:05:03 +0330

    Add prettier related stuff & code reformat

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
 import { title } from "@/src/components/primitives";
 
 export default function AboutPage() {
-  return (
-    <div>
-      <h1 className={title()}>About</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className={title()}>About</h1>
+        </div>
+    );
 }
--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,9 +1,9 @@
 import { title } from "@/src/components/primitives";
 
 export default function DocsPage() {
-  return (
-    <div>
-      <h1 className={title()}>Docs</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className={title()}>Docs</h1>
+        </div>
+    );
 }
