8afe3445347f92df86dedcc62dfd08b6ff82b926
AmRo <VirtualEmailBox045@Gmail.Com>
2025-04-26 21:31:52 +0330

    Revert output dir to default

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    distDir: "build",
 
     images: {
         unoptimized: true
-    },
-    experimental: {
-        scrollRestoration: true
     }
 };
 
